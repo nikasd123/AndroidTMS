cb8707e61e0ec1e834e1d6fedfcd5c3af93e53f4
nikita <nikita_eremeev.95@mail.ru>
2023-04-10 14:03:19 +0300

    Lists_Activity_TMS

--- app/src/main/java/com/example/tmsandroid/MainActivity.kt
+++ app/src/main/java/com/example/tmsandroid/MainActivity.kt
@@ -17,26 +17,5 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    private fun lists() {
-        val students = listOf("Sergey", "Nikita", "Sasha")
-        val animals = mutableSetOf("Cat", "Dog", "Bear")
-        animals.add("Parrot")
-        val vocabulary = mapOf(
-            "Hello" to "Привет",
-            "World" to "Мир"
-        )
 
-        students.forEach {
-            Log.d("TEST_ANDROID", "Student: $it")
-        }
-
-        animals.forEach{
-            Log.d("TEST_ANDROID", "Animal: $it")
-        }
-
-        Log.d("TEST_ANDROID", vocabulary.entries.joinToString {
-            "${it.key}: ${it.value}"
-        })
-
-    }
 }
