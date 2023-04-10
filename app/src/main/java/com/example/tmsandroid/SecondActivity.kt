764833b1fb080ff3882bfa5fb63d203d4012f9e1
nikita <nikita_eremeev.95@mail.ru>
2023-04-10 13:51:44 +0300

    Lists_Activity_TMS

--- app/src/main/java/com/example/tmsandroid/SecondActivity.kt
+++ app/src/main/java/com/example/tmsandroid/SecondActivity.kt
@@ -11,7 +11,5 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-
-        
     }
 }
