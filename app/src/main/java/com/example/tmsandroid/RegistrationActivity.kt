5ba2bb58e50d60003dd76261f7c6b81af22d8003
nikita <nikita_eremeev.95@mail.ru>
2023-04-11 20:38:48 +0300

    RegistrationActivity

---
+++ app/src/main/java/com/example/tmsandroid/RegistrationActivity.kt
@@ -0,0 +1,52 @@
+package com.example.tmsandroid
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.registration_window)
+
+        passwordLengthCheck()
+    }
+
+    private fun passwordLengthCheck() {
+        val button = findViewById<Button>(R.id.button)
+
+        // val passwordText = findViewById<EditText>(R.id.inputPassword)
+        // val passwordLength = passwordText.text.length
+
+        val toSecondActivity = Intent(this, SecondActivity::class.java)
+        val passwordText = findViewById<EditText>(R.id.inputPassword)
+
+        passwordText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // This method is called to notify you that, within s, the count characters beginning at start are about to be replaced by new text with length after.
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // This method is called to notify you that, within s, the count characters beginning at start have just replaced old text that had length before.
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // This method is called to notify you that, somewhere within s, the text has been changed.
+                val passwordLength = passwordText.text.length
+                button.isEnabled = false
+
+                if (passwordLength > 8) {
+                    button.isEnabled = true
+                    button.setOnClickListener {
+                        startActivity(toSecondActivity)
+                        onStop()
+                    }
+                }
+            }
+        })
+    }
+}
