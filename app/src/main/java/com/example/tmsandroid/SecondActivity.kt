048aebb5f0b8e28e83165711495ab3b2645364a9
nikita <nikita_eremeev.95@mail.ru>
2023-04-16 23:40:25 +0300

    task4

--- app/src/main/java/com/example/tmsandroid/SecondActivity.kt
+++ app/src/main/java/com/example/tmsandroid/SecondActivity.kt
@@ -6,6 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.second_window)
     }
 }
