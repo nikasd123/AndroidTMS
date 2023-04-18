7e5d6298578bea0cebf886e1f26ebb3cf584ba5c
nikita <nikita_eremeev.95@mail.ru>
2023-04-18 15:15:25 +0300

    task4

--- app/src/main/java/com/example/tmsandroid/SecondActivity.kt
+++ app/src/main/java/com/example/tmsandroid/SecondActivity.kt
@@ -2,10 +2,16 @@ package com.example.tmsandroid
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.tmsandroid.fragments.SpinnerFragment
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.second_window)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.spinnerFragment, SpinnerFragment.newInstance())
+            .commit()
     }
 }
