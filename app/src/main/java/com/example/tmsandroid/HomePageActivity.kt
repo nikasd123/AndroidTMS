f481dae2a15adfc7a654a3f0d26dc3f02b166e89
nikita <nikita_eremeev.95@mail.ru>
2023-04-30 12:54:09 +0300

    HomeWork 5 Recycler

--- app/src/main/java/com/example/tmsandroid/HomePageActivity.kt
+++ app/src/main/java/com/example/tmsandroid/HomePageActivity.kt
@@ -2,13 +2,12 @@ package com.example.tmsandroid
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.tmsandroid.fragments.ChatsFragment
 import com.example.tmsandroid.fragments.RegistrationFragment
 
 class HomePageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.second_window)
+        setContentView(R.layout.main_window)
 
         supportFragmentManager
             .beginTransaction()
--- app/src/main/java/com/example/tmsandroid/fragments/RegistrationFragment.kt
+++ app/src/main/java/com/example/tmsandroid/fragments/RegistrationFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.tmsandroid.R
 import com.example.tmsandroid.databinding.RegistrationFragmentBinding
 
 class RegistrationFragment : Fragment() {
@@ -16,7 +17,7 @@ class RegistrationFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = RegistrationFragmentBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -45,10 +46,21 @@ class RegistrationFragment : Fragment() {
 
                 if (passwordLength > 8) {
                     binding.button.isEnabled = true
+//                    binding.button.setOnClickListener {
+//                        val action = MyFragmentDirections.actionMyFragmentToAnotherFragment()
+//                        val navController = Navigation.findNavController(requireView())
+//                        navController.navigate(action)
+//                    }
+
+                    val fragmentManager = requireActivity().supportFragmentManager
+
                     binding.button.setOnClickListener {
-                        val action = MyFragmentDirections.actionMyFragmentToAnotherFragment()
-                        val navController = Navigation.findNavController(requireView())
-                        navController.navigate(action)
+                        val fragment = ChatsFragment.newInstance()
+                        val transaction = fragmentManager.beginTransaction()
+
+                        transaction.replace(R.id.registration_fragment, fragment)
+                        transaction.addToBackStack(null)
+                        transaction.commit()
                     }
                 }
             }
